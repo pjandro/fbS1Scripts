5b913663c69686fcc58d2aef507ff0a59f12c829
Ilia Korshunov <ikorshunov@uzum.com>
2025-06-15 17:48:15 +0300

    add tgBot starting file

---
+++ adds/tgBot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/gem/moonraker-telegram-bot
+/home/gem/moonraker-telegram-bot-env/bin/python /home/gem/moonraker-telegram-bot/bot/main.py -c /home/gem/printer_data/config/telegram-bot.cfg -l /tmp/tgBot.log
