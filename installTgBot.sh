e18b401850a7d0a4e726605082ae1597f2f24917
Ilia Korshunov <ikorshunov@uzum.com>
2025-06-15 18:14:11 +0300

    update

--- installTgBot.sh
+++ installTgBot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+	echo "You must provide your apikey"
+	exit 1
+fi
 cat << EOF | chroot /server/tmp /bin/bash
 apt install python3-virtualenv automake libtool libwebp-dev
 virtualenv -p /usr/bin/python3 --system-site-package /home/gem/moonraker-telegram-bot-env/
@@ -10,6 +14,7 @@ git clone https://github.com/nlef/moonraker-telegram-bot.git
 cat /home/gem/moonraker-telegram-bot/scripts/requirements.txt | grep -v "uvloop" > /home/gem/space/requirements.txt
 /home/gem/moonraker-telegram-bot-env/bin/pip install --no-cache-dir -r /home/gem/space/requirements.txt
 cp /home/gem/moonraker-telegram-bot/scripts/base_install_template /home/gem/printer_data/config/telegram-bot.cfg
+sed -i '/AweSomeBotToken/c\bot_token: $1\' /home/gem/printer_data/config/telegram-bot.cfg
 chown gem:gem /home/gem/printer_data/config/telegram-bot.cfg
 rm -rf ./space/
 wget https://raw.githubusercontent.com/pjandro/fbS1Scripts/refs/heads/main/adds/tgBot.sh
@@ -18,3 +23,4 @@ cp ./kp.sh kp.sh_back
 wget https://raw.githubusercontent.com/pjandro/fbS1Scripts/refs/heads/main/adds/kp.sh
 chmod +x ./kp.sh
 EOF
+sync && reboot
