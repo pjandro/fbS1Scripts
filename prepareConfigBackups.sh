96cf46844b21cd82efcf28670fd5dada871df213
Ilya Korshunov <pjandro@yandex.ru>
2025-06-24 03:19:07 +0300

    compress backup files

--- prepareConfigBackups.sh
+++ prepareConfigBackups.sh
@@ -10,10 +10,10 @@ if [ -f ./telegram-bot.cfg ]; then
 fi
 /home/gem/moonraker-env/bin/python3 /home/gem/moonraker/scripts/dbtool.py backup /home/gem/printer_data/database/ /tmp/backup/moonraker.db
 cd /tmp
-tar -cpv ./backup > backup.tar
-if [ -f /home/gem/fluidd/backup.tar ]; then
-  rm /home/gem/fluidd/backup.tar
+tar -cpv ./backup | xz -c8 > backup.tar.xz
+if [ -f /home/gem/fluidd/backup.tar.xz ]; then
+  rm /home/gem/fluidd/backup.tar.xz
 fi
-mv backup.tar /home/gem/fluidd/
+mv backup.tar.xz /home/gem/fluidd/
 rm -rf ./backup
 EOF
