874662fbd8387a518794ba41e5cc34c4780042aa
Ilya Korshunov <pjandro@yandex.ru>
2025-06-24 22:35:31 +0300

    fix

--- installTgBot.sh
+++ installTgBot.sh
@@ -9,6 +9,8 @@ cd /home/gem
 wget https://github.com/pjandro/fbS1Scripts/raw/refs/heads/main/bins/tgBot.tar
 tar xfvp tgBot.tar
 rm tgBot.tar
+sync
+sleep 2
 cp /home/gem/moonraker-telegram-bot/scripts/base_install_template /home/gem/printer_data/config/telegram-bot.cfg
 sed -i '/AweSomeBotToken/c\bot_token: $1\' /home/gem/printer_data/config/telegram-bot.cfg
 chown gem:gem /home/gem/printer_data/config/telegram-bot.cfg
@@ -18,4 +20,4 @@ mv ./kp.sh kp.sh_back
 wget https://raw.githubusercontent.com/pjandro/fbS1Scripts/refs/heads/main/adds/kp.sh
 chmod +x ./kp.sh
 EOF
-sync && reboot
+sync && sleep 2 && reboot
