c85c1e9d83d1ab70c93df6406298017e0d8b8a99
Ilya Korshunov <pjandro@yandex.ru>
2025-06-24 03:10:05 +0300

    add script for creating backup of configs and database

---
+++ prepareConfigBackups.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cat << EOF | chroot /server/tmp /bin/bash
+cd /tmp
+mkdir backup
+cd /home/gem/printer_data/config/
+cp printer.cfg /tmp/backup
+cp printer_config.cfg /tmp/backup
+if [ -f ./telegram-bot.cfg ]; then
+  cp telegram-bot.cfg /tmp/backup
+fi
+/home/gem/moonraker-env/bin/python3 /home/gem/moonraker/scripts/dbtools.py backup /home/gem/printer_data/database/ /tmp/backup/moonraker.db
+cd /tmp
+tar -cpv ./backup > backup.tar
+if [ -f /home/gem/fluidd/backup.tar ]; then
+  rm /home/gem/fluidd/backup.tar
+fi
+mv backup.tar /home/gem/fluidd/
+rm -rf ./backup
+EOF
