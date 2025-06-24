3717aabd3dd441fef0a45b9d56db9a8d3755aaab
Ilya Korshunov <pjandro@yandex.ru>
2025-06-24 04:45:38 +0300

    some changes

--- changeRunSh.sh
+++ changeRunSh.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cat << EOF | chroot /server/tmp /bin/bash
 cd /
+if [ -f ./run.sb_back ]; then
+  rm ./run.sb_back
+fi
 mv ./run.sh run.sh_back
 wget https://raw.githubusercontent.com/pjandro/fbS1Scripts/refs/heads/main/adds/run.sh
 chmod +x ./run.sh
